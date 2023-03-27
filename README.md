bb53248ebf883857df721f85392617c7d3e41f94
Simon.Chen <ice_mind@yeah.net>
2023-03-27 17:39:43 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TaoExchange
-1、A fast exchange
+1. A fast exchange
+2. Grpc v.153.0
+3. Protobuf 3.22.2
+
---
+++ protos/gen.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET BIN_DIR="..\\3rdp\\grpc\\bin"
+SET CPP_I_DIR="..\\facade\\include"
+SET CPP_SRC_DIR="..\\facade\\src"
+SET protoc=%BIN_DIR%\\protoc.exe
+
+%protoc% -I=./ --cpp_out=./ ./*.proto 
+move *.h %CPP_I_DIR%
+move *.cc %CPP_SRC_DIR%
